a08667f31d296a254774d653ae191626cb0ca93e
bazhinilya <bazhin.2210@mail.ru>
2024-12-21 02:48:54 +0300

    21.12.24 edit parseInputOutput, add incoming/outgoing, script/userRegex

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import 'dotenv/config.js'
 
 import fs from 'fs';
-import { getClass, getId, getInputOutput, getName } from './src/utils.js';
+import { getClass, getId, getIncoming, getInputOutput, getName, getOutgoing } from './src/utils.js';
 
 const xmlOriginal = fs.readFileSync(process.env.BPMN_FILE, 'utf8');
 
-const regexServiceTask = /<bpmn:serviceTask[\s\S]*?<\/bpmn:serviceTask>/gi;
-const serviceTasksMatch = xmlOriginal.match(regexServiceTask);
+const serviceTaskRegex = /<bpmn:serviceTask[\s\S]*?<\/bpmn:serviceTask>/gi;
+const scriptTaskRegex = /<bpmn:scriptTask[\s\S]*?<\/bpmn:scriptTask>/gi;
+const userTaskRegex = /<bpmn:userTask[\s\S]*?<\/bpmn:userTask>/gi;
+
+const serviceTasksMatch = xmlOriginal.match(serviceTaskRegex);
+const scriptTasksMatch = xmlOriginal.match(scriptTaskRegex);
+const userTasksMatch = xmlOriginal.match(userTaskRegex);
 
 const sts = [];
 for (const serviceTask of serviceTasksMatch) {
@@ -14,7 +19,9 @@ for (const serviceTask of serviceTasksMatch) {
         idMatch: getId(serviceTask),
         nameMatch: getName(serviceTask),
         classMatch: getClass(serviceTask),
-        inputOutputMatch: getInputOutput(serviceTask)
+        inputOutputMatch: getInputOutput(serviceTask),
+        incomingMatch: getIncoming(serviceTask),
+        outgoingMatch: getOutgoing(serviceTask),
     });
 }
 console.log(sts);
--- src/utils.js
+++ src/utils.js
@@ -1,34 +1,49 @@
-const regexId = /id="([^"]+)"/;
-const regexName = /name="([^"]+)"/;
-const regexClass = /camunda:class="([^"]+)"/;
-const regexInputOuput = /<camunda:inputOutput>[\s\S]*?<\/camunda:inputOutput>/gi;
-const regexInputParam = /<camunda:inputParameter name="([^"]+)"[^>]*>(.*?)<\/camunda:inputParameter>/gs;
-const regexOutputParam = /<camunda:outputParameter name="([^"]+)"[^>]*>(.*?)<\/camunda:outputParameter>/gs;
-const regexMapEntry = /<camunda:entry key="([^"]+)"[^>]*>(.*?)<\/camunda:entry>/gs;
-const regexInterpolation = /[\$\{\}]/g
+const idRegex = /id="([^"]+)"/;
+const nameRegex = /name="([^"]+)"/;
+const classRegex = /camunda:class="([^"]+)"/;
+const inputOuputRegex = /<camunda:inputOutput>[\s\S]*?<\/camunda:inputOutput>/gi;
+const incomingRegex = /<bpmn:incoming>[\s\S]*?<\/bpmn:incoming>/gi;
+const outgoingRegex = /<bpmn:outgoing>[\s\S]*?<\/bpmn:outgoing>/gi;
 
-export const getId = (strMatch) => strMatch.match(regexId)[1];
-export const getName = (strMatch) => strMatch.match(regexName)[1];
-export const getNames = (strMatch) => strMatch.match(regexName);
-export const getClass = (strMatch) => strMatch.match(regexClass)[1];
+const inputParamRegex = /<camunda:inputParameter name="([^"]+)"[^>]*>(.*?)<\/camunda:inputParameter>/gs;
+const outputParamRegex = /<camunda:outputParameter name="([^"]+)"[^>]*>(.*?)<\/camunda:outputParameter>/gs;
+const mapEntriesRegex = /<camunda:entry key="([^"]+)">([^<]+)<\/camunda:entry>/g;
+const scriptRegex = /<camunda:script[^>]*>([^<]+)<\/camunda:script>/;
 
-export const getInputOutput = (strMatch) => {
-    const inputOutputMatch = strMatch.match(regexInputOuput)[0];
+export const getId = (strMatch) => strMatch.match(idRegex)[1];
+export const getName = (strMatch) => strMatch.match(nameRegex)[1];
+export const getClass = (strMatch) => strMatch.match(classRegex)[1];
+export const getIncoming = (strMatch) => strMatch.match(incomingRegex).map(item => item.replace(/<\/?bpmn:incoming>/g, ''));
+export const getOutgoing = (strMatch) => strMatch.match(outgoingRegex).map(item => item.replace(/<\/?bpmn:outgoing>/g, ''))
+
+export function getInputOutput(strMatch) {
+    const inputOutputMatch = strMatch.match(inputOuputRegex)[0];
     return {
-        inputs: findAttr(inputOutputMatch, regexInputParam),
-        outputs: findAttr(inputOutputMatch, regexOutputParam),
-        mapEntry: findAttr(inputOutputMatch, regexMapEntry)
+        inputs: parseInputOutput(inputOutputMatch, inputParamRegex),
+        outputs: parseInputOutput(inputOutputMatch, outputParamRegex),
     };
 }
 
-function findAttr(inputOutputMatch, regex) {
-    const variables = [];
+function parseInputOutput(inputOutputMatch, regex) {
+    const taskVariables = [];
+
     let match;
     while ((match = regex.exec(inputOutputMatch)) !== null) {
-        if (match.index === regex.lastIndex) {
-            regex.lastIndex++;
+        const key = match[1];
+        let value = match[2].trim();
+
+        if (value.startsWith('<camunda:map>')) {
+            const mapValue = {};
+            let mapEntry;
+            while ((mapEntry = mapEntriesRegex.exec(value)) !== null) {
+                mapValue[mapEntry[1]] = mapEntry[2];
+            }
+            value = mapValue;
+        } else if (value.startsWith('<camunda:script')) {
+            const scriptContent = value.match(scriptRegex);
+            value = { script: scriptContent[1] };
         }
-        variables.push({ name: match[1], value: match[2].trim().replace(regexInterpolation, '') });
+        taskVariables.push({ key, value });
     }
-    return variables;
+    return taskVariables;
 }
